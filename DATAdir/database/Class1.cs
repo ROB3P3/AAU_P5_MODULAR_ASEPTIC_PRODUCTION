0fbe7a73179ae29b1ada808380b230da0604c04f
antonwarming1 <anton.warming.arent@gmail.com>
2024-11-14 12:44:36 +0100

    database shit

---
+++ DATAdir/database/Class1.cs
@@ -0,0 +1,177 @@
+﻿using Google.Protobuf.Collections;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace databaseSQL
+{
+    internal class database
+    {
+        private string _db_host;
+        private string _db_user;
+        private string _db_password;
+        private string _db_name;
+        private MySqlConnection mysql;
+
+
+        public database(string _db_host, string _db_user, string _db_password, string _db_name)
+        {
+            this._db_host = _db_host;
+            this._db_user = _db_user;
+            this._db_password = _db_password;
+            this._db_name = _db_name;
+            connection();
+        }
+
+
+        private void connection()
+        {
+            string myConnectionString = "server=" + this._db_host + ";uid=" + this._db_user + ";pwd=" + this._db_password + ";database=" + this._db_name;
+            mysql = new MySqlConnection(myConnectionString);
+            mysql.Open();
+            Console.WriteLine("open server");
+        }
+
+
+        public void create_table_order()
+        {
+            // Define your CREATE TABLE SQL query
+            string createTableOrder = @"
+                    CREATE TABLE IF NOT EXISTS hahah (
+                    order_number INT,
+                    amount INT,
+                    company VARCHAR(255),
+                    medicine_type VARCHAR(255)
+                );
+            ";
+            
+            MySqlCommand cmd = new MySqlCommand(createTableOrder, mysql);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("orderData table created");
+
+            
+
+        }
+
+
+        public void create_table_production()
+        {
+            // Define your CREATE TABLE SQL query
+            string createTableProduction = @"
+                CREATE TABLE IF NOT EXISTS nejnej (
+                order_number INT,
+                type_container VARCHAR(255),
+                amount_in_carrier INT,
+                start_time_full_system INT,
+                end_time_full_system INT,
+                Manufacturing_Lead_Time INT,
+                start_time_modul_1 INT,
+                end_time_modul_1 INT,
+                used_time_modul_1 INT,
+                start_time_modul_2 INT,
+                end_time_modul_2 INT,
+                used_time_modul_2 INT,
+                carrier_id INT,
+                modul_used VARCHAR(255)
+                );
+            ";
+
+       
+            MySqlCommand cmd = new MySqlCommand(createTableProduction, mysql);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("table production created ");
+
+
+           
+
+        }
+
+        public void insert_data_order(int order_number, string type_container, int amount_in_carrier,
+                    int start_time_full_system, int end_time_full_system,
+                    int manufacturing_lead_time, int start_time_modul_1,
+                    int end_time_modul_1, int used_time_modul_1, int start_time_modul_2,
+                    int end_time_modul_2, int used_time_modul_2, int carrier_id, string modul_used)
+        {
+            string insertData= @"
+            INSERT INTO nejnej (
+                order_number, type_container, amount_in_carrier,
+                start_time_full_system, end_time_full_system,
+                Manufacturing_Lead_Time, start_time_modul_1, end_time_modul_1,
+                used_time_modul_1, start_time_modul_2, end_time_modul_2,
+                used_time_modul_2, carrier_id, modul_used
+            )
+            VALUES (
+                @orderNumber, @typeContainer, @amountInCarrier,
+                @startTimeFullSystem, @endTimeFullSystem,
+                @manufacturingLeadTime, @startTimeModule1, @endTimeModule1,
+                @usedTimeModule1, @startTimeModule2, @endTimeModule2,
+                @usedTimeModule2, @carrierId, @moduleUsed
+            );";
+            // Ensure the connection is open before executing the command
+            
+
+            MySqlCommand cmd = new MySqlCommand(insertData, mysql);
+
+            cmd.Parameters.AddWithValue("orderNumber", order_number);
+            cmd.Parameters.AddWithValue("typeContainer", type_container);
+            cmd.Parameters.AddWithValue("amountInCarrier", amount_in_carrier);
+            cmd.Parameters.AddWithValue("startTimeFullSystem", start_time_full_system);
+            cmd.Parameters.AddWithValue("endTimeFullSystem", end_time_full_system);
+            cmd.Parameters.AddWithValue("manufacturingLeadTime", manufacturing_lead_time);
+            cmd.Parameters.AddWithValue("startTimeModule1", start_time_modul_1);
+            cmd.Parameters.AddWithValue("endTimeModule1", end_time_modul_1);
+            cmd.Parameters.AddWithValue("usedTimeModule1", used_time_modul_1);
+            cmd.Parameters.AddWithValue("startTimeModule2", start_time_modul_2);
+            cmd.Parameters.AddWithValue("endTimeModule2", end_time_modul_2);
+            cmd.Parameters.AddWithValue("usedTimeModule2", used_time_modul_2);
+            cmd.Parameters.AddWithValue("carrierId", carrier_id);
+            cmd.Parameters.AddWithValue("moduleUsed", modul_used);
+
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Inserted Into order");
+            
+
+
+        }
+
+
+        public void insert_data_production(int order_number, int amount, string company,
+                    string medicine_type)
+        {
+            string insertDataProduction = @"
+            INSERT INTO hahah (
+                order_number, amount, company,
+                medicine_type
+            )
+            VALUES (
+                @orderNumber, @amount, @company,
+                @medicineType
+            );";
+            
+
+            MySqlCommand cmd = new MySqlCommand(insertDataProduction, mysql);
+
+            cmd.Parameters.AddWithValue("orderNumber", order_number);
+            cmd.Parameters.AddWithValue("amount", amount);
+            cmd.Parameters.AddWithValue("company", company);
+            cmd.Parameters.AddWithValue("medicineType", medicine_type);
+            
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Inserted Into production");
+            
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+       
+            
+    }
+}
---
+++ DATAdir/database/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace databaseSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string db_host = "localhost";
+            string db_user = "anton";
+            string db_password = "anton";
+            string db_name = "pets";  
+            
+            database db = new database(db_host, db_user, db_password, db_name);
+
+            db.create_table_order();
+
+            db.create_table_production();
+
+            db.insert_data_order(8, "string type_container", 3,
+                    2, 1,
+                    2, 1,
+                    1, 2, 1,
+                    1, 2, 2, "modul_used");
+
+            db.insert_data_production(2,2,"sus","df");
+
+            Console.WriteLine("jeg er færdig");
+
+
+        }
+    }
+}
