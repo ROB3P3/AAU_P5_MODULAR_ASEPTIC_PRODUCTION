7c9e0108f3fa1f4f6039954dc829452daad1703f
Villiam <74359785+Vollenator@users.noreply.github.com>
2024-11-18 13:34:06 +0100

    Subscribed to AppState and CarrierID
    Added subscribers to Application state and carrier id, and made it react accordingly.

--- DATAdir/OPCUA-CLIENT/OPCUA/Program.cs
+++ DATAdir/OPCUA-CLIENT/OPCUA/Program.cs
@@ -10,11 +10,11 @@ using static System.Collections.Specialized.BitVector32;
 namespace OPCUA
 {
     class Program
-{
+    {
         static void Main(string[] args)
         {
             // Setup the endpoint URL
-            var endpointUrl = "opc.tcp://172.20.1.1:4840";
+            var endpointUrl = "opc.tcp://172.20.13.1:4840";
             // Create the application configuration
             var applicationConfiguration = CreateApplicationConfiguration();
             // Create the endpoint
@@ -24,9 +24,14 @@ namespace OPCUA
             // Connect to the server, read 2 nodes and write a new value to another node
             using (var client = Session.Create(applicationConfiguration, endpoint, false, "OPCUAClient", 60000, null, null).Result)
             {
-                DisplayNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.PLCid", "PLCid");
-                DisplayNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.AppType", "AppType");
-                ModifyNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.CarrierID", "CarrierID");
+                DisplayNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.AppType", "appType");
+                SubscribeNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.AppState", "appState");
+                SubscribeNodeValue(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.CarrierID", "carrierID");
+
+                // wait for the user to press a key
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+
             }
         }
 
@@ -65,75 +70,134 @@ namespace OPCUA
             return new ConfiguredEndpoint(null, selectedEndpoint, endpointConfiguration);
         }
 
-    private static void DisplayNodeValue(Session client, string nodeId, string variableName)
-    {
-        // Create a NodeId object from the string nodeId
-        NodeId node = new NodeId(nodeId);
+        private static void DisplayNodeValue(Session client, string nodeId, string variableName)
+        {
+            // Create a NodeId object from the string nodeId
+            NodeId node = new NodeId(nodeId);
 
-        // Read the value of the node
-        DataValue value = client.ReadValue(node);
+            // Read the value of the node
+            DataValue value = client.ReadValue(node);
 
-        // Display the value
-        Console.WriteLine("{0} is: {1}", variableName, value.Value);
+            // Display the value
+            Console.WriteLine("{0} is: {1}", variableName, value.Value);
+        }
+
+        private static void SubscribeNodeValue(Session client, string nodeId, string variableName)
+        {
+            // write which node we are subscribing to and if connection is successful
+            Console.WriteLine("Subscribing to {0}", variableName);
+
+            // Create a NodeId object from the string nodeId
+            NodeId node = new NodeId(nodeId);
+
+            // create a subscription to the ApplicationState node
+            var subscription = new Subscription(client.DefaultSubscription);
+
+            // create a monitored item for the ApplicationState node
+            var monitoredItem = new MonitoredItem(subscription.DefaultItem)
+            {
+                StartNodeId = node,
+                AttributeId = Attributes.Value
+            };
+
+            // add the monitored item to the subscription
+            subscription.AddItem(monitoredItem);
+
+            // create a callback for the DataChanged event
+            monitoredItem.Notification += (item, eventArgs) =>
+            {
+                // get the value of the ApplicationState node
+                var value = ((MonitoredItemNotification)eventArgs.NotificationValue).Value.Value;
+                // display the value
+                //Console.WriteLine("{0} is: {1}", variableName, value);
+                switch (variableName)
+                {
+                    case "carrierID":
+                        if ((ushort)value != 0)
+                        {
+                            Console.WriteLine("Carrier found, {0} is: {1}", variableName, value);
+                            CarrierHandler(client, "ns=2;s=|var|CECC-LK.Application.MODULE_PLC13_MAIN.ServerCommand", "ServerCommand", (ushort)value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Carrier not found");
+                        }
+                        break;
+                    case "appState":
+                        Console.WriteLine("Application state is: {0}", value);
+                        break;
+                }
+            };
+
+            // add the subscription to the session
+            client.AddSubscription(subscription);
+            // start the subscription
+            subscription.Create();
+
+            
+        }
+
+
+        private static void CarrierHandler(Session client, string nodeId, string variableName, ushort value)
+        {
+            Console.WriteLine("Carrier handler, carrier ID is {0}", value);
+
+            string serverCommand = "begin";
+            ModifyNodeValue(client, nodeId, variableName, serverCommand);
+
+        }
+
+        private static void ApplicationStateHandler(Session client, string nodeId, string variableName, string value)
+        {
+            return;
+        }
+
+
+
+        private static void ModifyNodeValue(Session client, string nodeId, string variableName, string newValue)
+        {
+            // Create a NodeId object from the string nodeId
+            NodeId node = new NodeId(nodeId);
+            WriteValueCollection nodesToWrite = new WriteValueCollection();
+
+            // write which node we are going to modify
+            Console.WriteLine("Modifying {0}", variableName);
+
+            // write the current value of the node
+            DataValue value = client.ReadValue(node);
+            Console.WriteLine("Current value is: {0}", value.Value);
+
+            // Create a WriteValue object
+            WriteValue writeValue = new WriteValue
+            {
+                NodeId = node,
+                AttributeId = Attributes.Value,
+                Value = new DataValue(new Variant(newValue))
+            };
+
+            // Add the WriteValue object to the WriteValueCollection
+            nodesToWrite.Add(writeValue);
+
+
+            // Write the new value to the node
+            client.Write(null, nodesToWrite, out StatusCodeCollection results, out DiagnosticInfoCollection diagnosticInfos);
+
+            // Check if the write was successful
+            if (StatusCode.IsGood(results[0]))
+            {
+                Console.WriteLine("Write succeeded");
+            }
+            else
+            {
+                Console.WriteLine("Write failed");
+
+                // Display the error message
+                Console.WriteLine("Error message: {0}", results[0]);
+
+                // Display the diagnostic information
+                Console.WriteLine("Diagnostic information: {0}", diagnosticInfos[0]);
+
+            }
+        }
     }
-
-    private static void ModifyNodeValue(Session client, string nodeId, string variableName)
-    {
-        // Create a NodeId object from the string nodeId
-        NodeId node = new NodeId(nodeId);
-        WriteValueCollection nodesToWrite = new WriteValueCollection();
-
-        // write which node we are going to modify
-        Console.WriteLine("Modifying {0}", variableName);
-
-        // write the current value of the node
-        DataValue value = client.ReadValue(node);
-        Console.WriteLine("Current value is: {0}", value.Value);
-
-
-        // Ask user for new value, allow only uint16 values, otherwise try again
-        Console.WriteLine("Enter new uint16 value: ");
-        string newValueString = Console.ReadLine();
-        ushort newValue;
-        while (!ushort.TryParse(newValueString, out newValue))
-        {
-            Console.WriteLine("Invalid value, please enter a valid uint16 value: ");
-            newValueString = Console.ReadLine();
-        }
-
-
-
-        // Create a WriteValue object
-        WriteValue writeValue = new WriteValue
-        {
-            NodeId = node,
-            AttributeId = Attributes.Value,
-            Value = new DataValue(new Variant(newValue))
-        };
-
-        // Add the WriteValue object to the WriteValueCollection
-        nodesToWrite.Add(writeValue);
-
-
-        // Write the new value to the node
-        client.Write(null, nodesToWrite, out StatusCodeCollection results, out DiagnosticInfoCollection diagnosticInfos);
-
-        // Check if the write was successful
-        if (StatusCode.IsGood(results[0]))
-        {
-            Console.WriteLine("Write succeeded");
-        }
-        else
-        {
-            Console.WriteLine("Write failed");
-
-            // Display the error message
-            Console.WriteLine("Error message: {0}", results[0]);
-
-            // Display the diagnostic information
-            Console.WriteLine("Diagnostic information: {0}", diagnosticInfos[0]);
-
-        }
-    }
-}
 }
